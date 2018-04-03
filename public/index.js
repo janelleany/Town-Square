4c3e63aac560d043ffe2c8193a2e1edc6a2df655
Aaron G <aaronhgross@gmail.com>
2018-04-03 14:41:15 -0400

    working video and form submit to server

--- public/index.js
+++ public/index.js
@@ -1,30 +1,26 @@
-let recordVideo = function (vidId) {
+let recordVideo = () => {
 
     let preview = document.getElementById("preview");
     let recording = document.getElementById("recording");
     let recordButton = document.getElementById("recordButton");
     let stopButton = document.getElementById("stopButton");
-    let logElement = document.getElementById("log");
-    let leftDiv = document.querySelector(".left")
+    let showVideoSelector = document.querySelector('.showVideo')
+    let recordVideoSelector = document.querySelector('.recordVideo')
+    let submitThreadSelector = document.querySelector('.submit_thread')
 
-    //sets time to record
-    let recordingTimeMS = 5000;
 
-    function log(msg) {
-        logElement.innerHTML += msg + "\n";
-    }
-    //sets wait function
+    let recordingTimeMS = 1000;
+
     function wait(delayInMS) {
         return new Promise(resolve => setTimeout(resolve, delayInMS));
     }
-    // starts recording for 5 seconds or untill stopped
+
     function startRecording(stream, lengthInMS) {
         let recorder = new MediaRecorder(stream);
         let data = [];
 
         recorder.ondataavailable = event => data.push(event.data);
         recorder.start();
-        log(recorder.state + " for " + (lengthInMS / 1000) + " seconds...");
 
         let stopped = new Promise((resolve, reject) => {
             recorder.onstop = resolve;
@@ -46,46 +42,63 @@ let recordVideo = function (vidId) {
         stream.getTracks().forEach(track => track.stop());
     }
 
-    // adds click event to recordbutton 
     recordButton.addEventListener("click", function () {
-        //gets okay from user to allow video and audio
+            startRecording(preview.captureStream(), recordingTimeMS)
+            .then(recordedChunks => {
+
+                let recordedBlob = new Blob(recordedChunks, {
+                    type: "video/webm"
+                });
+
+                let formData = new FormData()
+                formData.append('video', recordedBlob)
+                recording.src = URL.createObjectURL(recordedBlob);
+                submitThread(formData);
+
+            }).then(showPreview => {
+                recordVideoSelector.className = 'recordVideo viewable-off'
+                showVideoSelector.className = 'showVideo viewable-on'
+                
+            })
+    }, false);
+
+    stopButton.addEventListener("click", function () {
+        stop(preview.srcObject);
+    }, false);
+}
+
+let startPreview = () => {
         navigator.mediaDevices.getUserMedia({
                 video: true,
                 audio: true
             }).then(stream => {
                 preview.srcObject = stream;
-                // downloadButton.href = stream;
                 preview.captureStream = preview.captureStream || preview.mozCaptureStream;
                 return new Promise(resolve => preview.onplaying = resolve);
-            }).then(() => startRecording(preview.captureStream(), recordingTimeMS))
-            // runs function to allow download
-            .then(recordedChunks => {
-                leftDiv.className = "left viewable-off";
-                // encodes video into webm 
-                let recordedBlob = new Blob(recordedChunks, {
-                    type: "video/webm"
-                });
-                // create post request with form data to localhost:3000/videos
-                let formData = new FormData()
-                console.log(recordedBlob)
-                formData.append('video', recordedBlob)
-
-                for (var pair of formData.entries()) {
-                    console.log(pair[0] + ', ' + pair[1]);
-                }
-
-                console.log('about to fetch')
-                fetch('http://localhost:3000', {
-                    method: 'POST',
-                    body: 'fd',formData
-                })
-
-                // creates url object
-                recording.src = URL.createObjectURL(recordedBlob);
             })
-            .catch(log);
-    }, false);
-    stopButton.addEventListener("click", function () {
-        stop(preview.srcObject);
-    }, false);
 }
+
+let submitThread = (form) => {
+    let submitThreadFormSelector = document.querySelector('form')
+
+    submitThreadFormSelector.addEventListener('submit', (event) => {
+        let threadTitleSelector = document.getElementById('thread_title').value;
+        let threadUsernameSelector = document.getElementById('user').value;
+
+        form.append('threadtitle', threadTitleSelector);
+        form.append('username', threadUsernameSelector);
+
+        event.preventDefault();
+        console.log('Fetching....')
+        fetch('http://localhost:3000/video', {
+            method: 'POST',
+            body: form 
+            // headers: {"Content-Type":"application/json"}
+        })
+        console.log('Done fetching')
+    })
+}
+
+startPreview();
+recordVideo();
+
